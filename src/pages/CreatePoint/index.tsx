04af0f959438b5cee3cbae9559a527e4526832e7
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-03 20:28:03 -0300

    buscando itens de coleta da api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    });
+  }, [])
+
   return (
     <div id="page-create-point">
       <header>
@@ -93,30 +108,13 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias"/>
-              <span>Pilhas e Baterias</span>
-            </li>
+            {items.map(item => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
+            
           </ul>
         </fieldset>
 
